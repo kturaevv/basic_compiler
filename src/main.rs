8beac8fd876181301ae3f719f90958ef3f630d49
kturaevv <kturaevv@gmail.com>
2024-05-23 14:00:52 +0500

    refactor: some improvements

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
+use basic_compiler::Config;
 use std::env;
-use std::fs;
-use std::process::exit;
+
+use basic_compiler::run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() > 2 {
-        println!("No more than 1 argument is accepted!");
-        exit(1)
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problems parsing out the arguments: {err}!");
+        std::process::exit(1)
+    });
+
+    if let Err(e) = run(config) {
+        println!("Something went wrong here! {e}");
+        std::process::exit(1)
     }
-
-    let file_path = &args[1];
-    let contents = fs::read_to_string(file_path).expect("Failed!");
-
-    println!("{:?}", contents);
 }
